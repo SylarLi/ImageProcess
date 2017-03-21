964fbdf21806ca368764acf269c686935809ffda
SylarLi <328403302@qq.com>
2017-03-21 15:14:35 +0800

    upload

--- ImageProcess/EdgeDetection/Canny.cs
+++ ImageProcess/EdgeDetection/Canny.cs
@@ -40,14 +40,14 @@ public class Canny
         double[,] angles = null;
         byte[,] nms = null;
         byte[] pixels = null;
-        BitmapData fromData = from.LockBits(new Rectangle(0, 0, from.Width, from.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+        BitmapData fromData = from.LockBits(new Rectangle(0, 0, from.Width, from.Height), ImageLockMode.ReadOnly, from.PixelFormat);
         LoadSource(fromData, out source);
         SobelFilter(fromData, source, out gradients, out angles);
         NoneMaximumSuppression(fromData, gradients, angles, out nms);
         EdgeTracking(fromData, nms);
         NMSToPixels(fromData, nms, out pixels);
         to = new Bitmap(from.Width, from.Height, from.PixelFormat);
-        BitmapData toData = to.LockBits(new Rectangle(0, 0, to.Width, to.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+        BitmapData toData = to.LockBits(new Rectangle(0, 0, to.Width, to.Height), ImageLockMode.WriteOnly, to.PixelFormat);
         IntPtr toPtr = toData.Scan0;
         System.Runtime.InteropServices.Marshal.Copy(pixels, 0, toPtr, pixels.Length);
         from.UnlockBits(fromData);
@@ -59,6 +59,8 @@ public class Canny
     {
         IntPtr fromPtr = fromData.Scan0;
         int byteSize = Math.Abs(fromData.Stride) * fromData.Height;
+        int bbp = fromData.Stride / fromData.Width;
+        if (bbp < 3) throw new NotSupportedException();
         source = new byte[byteSize];
         System.Runtime.InteropServices.Marshal.Copy(fromPtr, source, 0, source.Length);
     }
@@ -72,6 +74,7 @@ public class Canny
     /// <param name="angles"></param>
     private void SobelFilter(BitmapData bmpData, byte[] source, out double[,] gradients, out double[,] angles)
     {
+        int bbp = bmpData.Stride / bmpData.Width;
         gradients = new double[bmpData.Width, bmpData.Height];
         angles = new double[bmpData.Width, bmpData.Height];
         int maxH = bmpData.Height - 2;
@@ -92,14 +95,13 @@ public class Canny
                     int gbi = gj * bmpData.Stride;
                     for (int gi = gimin; gi <= gimax; gi++)
                     {
-                        int gii = gbi + gi * 3;
+                        int gii = gbi + gi * bbp;
                         int fx = gi + 1 - i;
                         int fy = gj + 1 - j;
                         gx += source[gii] * Gx[fy, fx];
                         gy += source[gii] * Gy[fy, fx];
                     }
                 }
-                int ii = bi + i * 3;
                 double magnitude = Math.Sqrt(gx * gx + gy * gy);
                 double angle = Math.Atan2(gy, gx) / Math.PI * 180;
                 if (angle < 0) angle = angle + 180;
@@ -126,7 +128,6 @@ public class Canny
             int bi = j * bmpData.Stride;
             for (int i = 1; i <= maxW; i++)
             {
-                int ii = bi + i * 3;
                 double magnitude = gradients[i, j];
                 double angle = angles[i, j];
                 double v1 = 0;
@@ -202,13 +203,17 @@ public class Canny
 
     private void NMSToPixels(BitmapData bmpData, byte[,] nms, out byte[] pixels)
     {
-        pixels = new byte[bmpData.Stride * bmpData.Height];
+        IntPtr bmpPtr = bmpData.Scan0;
+        int byteSize = Math.Abs(bmpData.Stride) * bmpData.Height;
+        int bbp = bmpData.Stride / bmpData.Width;
+        pixels = new byte[byteSize];
+        System.Runtime.InteropServices.Marshal.Copy(bmpPtr, pixels, 0, pixels.Length);
         for (int j = 0; j < bmpData.Height; j++)
         {
             int bi = j * bmpData.Stride;
             for (int i = 0; i < bmpData.Width; i++)
             {
-                int ii = bi + i * 3;
+                int ii = bi + i * bbp;
                 if (nms[i, j] == 2)
                 {
                     pixels[ii] = pixels[ii + 1] = pixels[ii + 2] = 255;
--- ImageProcess/EdgeDetection/Program.cs
+++ ImageProcess/EdgeDetection/Program.cs
@@ -9,50 +9,7 @@ namespace EdgeDetection
     {
         static void Main(string[] args)
         {
-            TestGrey();
-            TestBlur();
             TestCanny();
-            Console.In.ReadLine();
-        }
-
-        static void TestGrey()
-        {
-            GreyScale grey = new GreyScale();
-            string sourcePath = "../../../Resources/source.png";
-            string outputPath = "../../../Resources/grey.png";
-            Bitmap source = new Bitmap(sourcePath);
-            Bitmap output = null;
-            DateTime t1 = DateTime.Now;
-            if (grey.ProcessMemory(source, out output))
-            {
-                output.Save(outputPath);
-                Process process = Process.Start("explorer.exe", Path.GetFullPath(outputPath));
-                process.Exited += (object sender, EventArgs e) => output.Dispose();
-            }
-            DateTime t2 = DateTime.Now;
-            TimeSpan d = t2 - t1;
-            Console.WriteLine(string.Format("image size: {0} X {1}, process time: {2}ms", source.Width, source.Height, d.TotalMilliseconds));
-            source.Dispose();
-        }
-
-        static void TestBlur()
-        {
-            GaussianBlur blur = new GaussianBlur(5, 1.4f);
-            string sourcePath = "../../../Resources/grey.png";
-            string outputPath = "../../../Resources/blur.png";
-            Bitmap source = new Bitmap(sourcePath);
-            Bitmap output = null;
-            DateTime t1 = DateTime.Now;
-            if (blur.Process(source, out output))
-            {
-                output.Save(outputPath);
-                Process process = Process.Start("explorer.exe", Path.GetFullPath(outputPath));
-                process.Exited += (object sender, EventArgs e) => output.Dispose();
-            }
-            DateTime t2 = DateTime.Now;
-            TimeSpan d = t2 - t1;
-            Console.WriteLine(string.Format("image size: {0} X {1}, process time: {2}ms", source.Width, source.Height, d.TotalMilliseconds));
-            source.Dispose();
         }
 
         static void TestCanny()
