a0e3faf9319fdc1ee78793a864fd2864f16667eb
SylarLi <328403302@qq.com>
2017-03-21 15:12:58 +0800

    upload

---
+++ ImageProcess/Clutters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+
+namespace Clutters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestWhiteNoise();
+        }
+
+        private static void TestWhiteNoise()
+        {
+            WhiteNoise noise = new WhiteNoise();
+            string outputPath = "../../../Resources/white-noise-map.png";
+            Bitmap output = null;
+            DateTime t1 = DateTime.Now;
+            if (noise.Process(512, 512, out output))
+            {
+                output.Save(outputPath);
+                Process process = Process.Start("explorer.exe", Path.GetFullPath(outputPath));
+                process.Exited += (object sender, EventArgs e) => output.Dispose();
+            }
+            DateTime t2 = DateTime.Now;
+            TimeSpan d = t2 - t1;
+            Console.WriteLine(string.Format("image size: {0} X {1}, process time: {2}ms", output.Width, output.Height, d.TotalMilliseconds));
+        }
+    }
+}
---
+++ ImageProcess/Clutters/WhiteNoise.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+public class WhiteNoise
+{
+    public WhiteNoise()
+    {
+
+    }
+
+    /// <summary>
+    /// 生成随机白噪声图片
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    /// <param name="to"></param>
+    /// <returns></returns>
+    public bool Process(int width, int height, out Bitmap to)
+    {
+        Random rnd = new Random();
+        int bbp = 3;
+        int byteSize = width * height * bbp;
+        int stride = width * bbp;
+        byte[] values = new byte[byteSize];
+        for (int j = 0; j < height; j++)
+        {
+            int bi = j * stride;
+            for (int i = 0; i < width; i++)
+            {
+                // BGR
+                int ii = bi + i * bbp;
+                byte number = (byte)(rnd.Next() * 255);
+                values[ii + 2] = number;
+                values[ii + 1] = number;
+                values[ii] = number;
+            }
+        }
+        to = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+        BitmapData toData = to.LockBits(new Rectangle(0, 0, to.Width, to.Height), ImageLockMode.WriteOnly, to.PixelFormat);
+        IntPtr toPtr = toData.Scan0;
+        System.Runtime.InteropServices.Marshal.Copy(values, 0, toPtr, values.Length);
+        to.UnlockBits(toData);
+        return true;
+    }
+}
